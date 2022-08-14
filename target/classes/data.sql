698c22585168b158acfc0320ade8663797359b67
tusharnath10 <tusharnath10@gmail.com>
2022-08-14 23:31:34 +0530

    initial commit

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
---insert into role (name) SELECT 'ROLE_USER' from role having count(*) < 3
---insert into role (name) SELECT 'ROLE_MODERATOR' from role having count(*) < 3
---insert into role (name) SELECT 'ROLE_ADMIN' from role having count(*) < 3
